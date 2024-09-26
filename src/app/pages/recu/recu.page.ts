6cd36bfe9ca9e80717f47f2fcaf845e4e5ee4ee8
Clifforiano <cli.ramirez@duocuc.cl>
2024-09-26 12:33:53 -0300

    espacios recu
    borrar espacios en recu

--- src/app/pages/recu/recu.page.ts
+++ src/app/pages/recu/recu.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router'; // Asegúrate de importar Router
 
 @Component({
   selector: 'app-recu',
@@ -10,8 +11,7 @@ export class RecuPage implements OnInit {
 
   public form_recu: FormGroup;
 
-  constructor(private fb: FormBuilder) {
-    // Crear el FormGroup con validaciones
+  constructor(private fb: FormBuilder, private router: Router) { // Agregar Router al constructor
     this.form_recu = this.fb.group({
       correo: ['', [Validators.required, Validators.email, this.validarDominio]], // Correo
     });
@@ -31,8 +31,19 @@ export class RecuPage implements OnInit {
     if (this.form_recu.valid) {
       console.log('Correo válido:', this.form_recu.value);
       // Aquí puedes agregar la lógica para enviar el correo de recuperación
+      
+      // Redirigir a la página deseada
+      this.router.navigate(['/cambiarclave2']); // Cambia la ruta si es necesario
     } else {
       console.log('Formulario no válido');
+    }
+  }
+
+  // Método para eliminar espacios de un control específico
+  removeSpaces(controlName: string) {
+    const control = this.form_recu.get(controlName);
+    if (control) {
+      control.setValue(control.value.replace(/\s+/g, ''));
     }
   }
 }
